cc2d3eb2c5f4f7b78f19a11f8d9ad7d84801b416
brandoncryu <chryu1@gmail.com>
2020-06-05 23:18:34 -0500

    home page, navigation, blog list complete

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { BlogListComponent } from './blog-list/blog-list.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:  "", pathMatch:  "full",redirectTo:  "home"},
+  {path: "home", component: HomeComponent},
+  {path: "blog-list", component: BlogListComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { BannerComponent } from './banner/banner.component';
+import { BlogListComponent } from './blog-list/blog-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    BannerComponent,
+    BlogListComponent
   ],
   imports: [
     BrowserModule,
